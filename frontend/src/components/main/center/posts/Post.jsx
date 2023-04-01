38e88544c0d53244673ea2e2282f9f4eb9c9fe82
Sachin Kumar <sachinyadav1469@gmail.com>
2023-04-01 14:38:53 +0530

    Linkeding home page

--- frontend/src/components/main/center/posts/Post.jsx
+++ frontend/src/components/main/center/posts/Post.jsx
@@ -5,6 +5,7 @@ function Post() {
   return (
     <div className='post'>
       <div className="post-profile">
+        
         <div className="profile-img">
             <img src="https://media.licdn.com/dms/image/C560BAQHTvZwCx4p2Qg/company-logo_100_100/0/1612205615891?e=1688601600&v=beta&t=efv4xvpx8Vs0tv6pXQLn5biOQKrf1ZQ14-GS10Knha8" alt="" />
         </div>
@@ -29,12 +30,50 @@ function Post() {
             {/* <button className='profile-dot-menu'></button> */}
             <button className='profile-follow-btn'>+ FOLLOW</button>
         </div>
+        
       </div>
-      <div className='post-text-content'>
-        <p></p>
+      <div className='post-text-content'> 
+        <p>
+        💡JavaScript Tip💡
+          <br/><br></br>
+          Use 𝘄𝗶𝗻𝗱𝗼𝘄.𝗵𝗶𝘀𝘁𝗼𝗿𝘆.𝗽𝘂𝘀𝗵𝗦𝘁𝗮𝘁𝗲 and 𝘄𝗶𝗻𝗱𝗼𝘄.𝗵𝗶𝘀𝘁𝗼𝗿𝘆.𝗿𝗲𝗽𝗹𝗮𝗰𝗲𝗦𝘁𝗮𝘁𝗲 methods to easily manipulate the browser history without refreshing the page.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum nostrum ipsam praesentium eveniet! Minus incidunt maiores veniam asperiores, non minima saepe sint voluptates officia culpa consectetur exercitationem quam. Officia, optio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, voluptatibus.
+        </p>
+        <button>...see more</button>
       </div>
       <div className="post-images">
-        <img src="" alt="" />
+        <img src="https://media.licdn.com/dms/image/D4E22AQHDtw1Y1r2htQ/feedshare-shrink_800/0/1680295629304?e=1683158400&v=beta&t=GAV4pchHO32piMfMWKNSRWu5sFWLEMQYvUOs_5q32JQ" alt="" />
+      </div>
+      <div className="post-interaction">
+        <div className="post-interaction-left">
+          <div className='post-interaction-icons'>
+            <img src='https://static.licdn.com/sc/h/8ekq8gho1ruaf8i7f86vd1ftt'/>
+            <img src='https://static.licdn.com/sc/h/41j9d0423ck1snej32brbuuwg'/>
+            <img src='https://static.licdn.com/sc/h/cpho5fghnpme8epox8rdcds22'/>
+          </div>
+          <a href='#'>19</a>
+        </div>
+        <div className="post-interaction-right">
+        </div>
+      </div>
+      <div className="post-interaction-buttons">
+        <button className='post-interaction-btn'>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"></path></svg>
+          <span>Like</span>
+        </button>
+        <button className='post-interaction-btn'>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 17 17" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g></g><path d="M15.5 0h-14c-0.827 0-1.5 0.673-1.5 1.5v10c0 0.827 0.673 1.5 1.5 1.5h0.5v4.102l4.688-4.102h8.812c0.827 0 1.5-0.673 1.5-1.5v-10c0-0.827-0.673-1.5-1.5-1.5zM16 11.5c0 0.275-0.224 0.5-0.5 0.5h-9.188l-3.312 2.898v-2.898h-1.5c-0.276 0-0.5-0.225-0.5-0.5v-10c0-0.275 0.224-0.5 0.5-0.5h14c0.276 0 0.5 0.225 0.5 0.5v10zM3 3h11v1h-11v-1zM3 5h11v1h-11v-1zM3 7h6v1h-6v-1z"></path></svg>
+          <span>Comment</span>
+        </button>
+        <button className='post-interaction-btn'>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M136 552h63.6c4.4 0 8-3.6 8-8V288.7h528.6v72.6c0 1.9.6 3.7 1.8 5.2a8.3 8.3 0 0 0 11.7 1.4L893 255.4c4.3-5 3.6-10.3 0-13.2L749.7 129.8a8.22 8.22 0 0 0-5.2-1.8c-4.6 0-8.4 3.8-8.4 8.4V209H199.7c-39.5 0-71.7 32.2-71.7 71.8V544c0 4.4 3.6 8 8 8zm752-80h-63.6c-4.4 0-8 3.6-8 8v255.3H287.8v-72.6c0-1.9-.6-3.7-1.8-5.2a8.3 8.3 0 0 0-11.7-1.4L131 768.6c-4.3 5-3.6 10.3 0 13.2l143.3 112.4c1.5 1.2 3.3 1.8 5.2 1.8 4.6 0 8.4-3.8 8.4-8.4V815h536.6c39.5 0 71.7-32.2 71.7-71.8V480c-.2-4.4-3.8-8-8.2-8z"></path></svg>
+          <span>Repost</span>
+        </button>
+        <button className='post-interaction-btn'>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M435.9 64.9l-367.1 160c-6.5 3.1-6.3 12.4.3 15.3l99.3 56.1c5.9 3.3 13.2 2.6 18.3-1.8l195.8-168.8c1.3-1.1 4.4-3.2 5.6-2 1.3 1.3-.7 4.3-1.8 5.6L216.9 320.1c-4.7 5.3-5.4 13.1-1.6 19.1l64.9 104.1c3.2 6.3 12.3 6.2 15.2-.2L447.2 76c3.3-7.2-4.2-14.5-11.3-11.1z"></path></svg>
+          <span>Send</span>
+        </button>
       </div>
     </div>
   )
--- frontend/src/components/main/right/RightMain.jsx
+++ frontend/src/components/main/right/RightMain.jsx
@@ -5,8 +5,74 @@ import "./rightMain.scss";
 
 function RightMain() {
   return (
-    <div className='right-main'>
-        RightMain
+    <div>
+      <div className='right-main'>
+          <h2>
+            <span>LinkedIn News</span> 
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" width="16" height="16" focusable="false">
+                <path d="M12 2H4a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V4a2 2 0 00-2-2zm-3 8v2H7.5A1.5 1.5 0 016 10.5a1.56 1.56 0 01.1-.5l1.08-3h2.13l-1.09 3zm0-3.75A1.25 1.25 0 1110.25 5 1.25 1.25 0 019 6.25z"></path>
+              </svg>
+            </span>
+          </h2>
+          <ul>
+              <a href="#">
+                <h3>Top newsLetters to follow</h3>
+                <p>Top news • 1,616 readers</p>
+              </a>
+              <a href="#">
+                <h3>Demand for office space soars</h3>
+                <p>2d ago • 1,492 readers</p>
+              </a>
+              <a href="#">
+                <h3>Shorter wait for US visas</h3>
+                <p>2d ago • 4,540 readers</p>
+              </a>
+              <a href="#">
+                <h3>Fresh IPO funding falls sharply</h3>
+                <p>20h ago</p>
+              </a>
+              <a href="#">
+                <h3>Hiring intent up in these sectors</h3>
+                <p>17h ago • 148 readers</p>
+              </a>
+          </ul>
+      </div>
+      <div className="right-main right-main-2">
+        <a href="#">
+          <img src="https://media.licdn.com/media/AAYQAgTPAAgAAQAAAAAAADVuOvKzTF-3RD6j-qFPqhubBQ.png" alt="" />
+        </a>
+      </div>
+      <div className="right-main-3">
+        <div className='linkedin-links'>
+          <a href="#">About</a>
+          <a href="#">Accessibility</a>
+          <a href="#">Help Center</a>
+        </div>
+        <div className='linkedin-links'>
+          <a href="#">Privacy & Terms</a>
+          <a href="#">Ad Choices</a>
+        </div>
+        <div className='linkedin-links'>
+          <a href="#">Advertising</a>
+          <a href="#">Business Services</a>
+        </div>
+        <div className='linkedin-links'>
+          <a href="#">Get the LinkedIn app</a>
+          <a href="#">More</a>
+        </div>
+      </div>
+      <div className='right-main-4'>
+        <p>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 14" data-supported-dps="56x14" fill="currentColor" className="mercado-match" width="56" height="14" focusable="false">
+              <g>
+                <path className="background-mercado" d="M22.1 8.2l3.09 3.8h-2.44L20 8.51V12h-2V2h2v5.88L22.54 5h2.55zm-8-3.4A2.71 2.71 0 0011.89 6V5H10v7h2V8.73a1.74 1.74 0 011.66-1.93C14.82 6.8 15 7.94 15 8.73V12h2V8.29c0-2.2-.73-3.49-2.86-3.49zM32 8.66a4.22 4.22 0 010 .44h-5.25v.07a1.79 1.79 0 001.83 1.43 2.51 2.51 0 001.84-.69l1.33 1a4.31 4.31 0 01-3.25 1.29 3.49 3.49 0 01-3.7-3.75 3.58 3.58 0 013.76-3.65C30.44 4.8 32 6.13 32 8.66zm-1.86-.86a1.46 1.46 0 00-1.59-1.4 1.64 1.64 0 00-1.8 1.4zM2 2H0v10h6v-2H2zm36 0h2v10h-1.89v-.7a2.44 2.44 0 01-2 .9 3.41 3.41 0 01-3.31-3.7 3.36 3.36 0 013.3-3.7 2.62 2.62 0 011.9.7zm.15 6.5a1.63 1.63 0 00-1.62-1.85A1.76 1.76 0 0034.9 8.5a1.76 1.76 0 001.63 1.85 1.63 1.63 0 001.62-1.85zM8 1.8A1.27 1.27 0 006.75 3a1.25 1.25 0 002.5 0A1.27 1.27 0 008 1.8zM7 12h2V5H7zM56 1v12a1 1 0 01-1 1H43a1 1 0 01-1-1V1a1 1 0 011-1h12a1 1 0 011 1zM46 5h-2v7h2zm.25-2a1.25 1.25 0 00-2.5 0 1.25 1.25 0 002.5 0zM54 8.29c0-2.2-.73-3.49-2.86-3.49A2.71 2.71 0 0048.89 6V5H47v7h2V8.73a1.74 1.74 0 011.66-1.93C51.82 6.8 52 7.94 52 8.73V12h2z"></path>
+              </g>
+            </svg>
+          </span> 
+          <span>© Sachin Kumar</span></p>
+      </div>
     </div>
   )
 }
